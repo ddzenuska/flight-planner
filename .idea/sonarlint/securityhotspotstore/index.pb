65418b9a26026cf60adc7f74fbb1ff0734b56152
ddzenuska <ddzenuska@gmail.com>
2024-05-23 18:35:25 +0300

    set up authorisation for test-01

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,5 @@
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
 z
 Jsrc/main/java/io/codelex/flightplanner/FlightPlannerSpringApplication.java,e/b/ebdb188f9d030c2e455c239b2c29ae2eba115d6c
+x
+Hsrc/main/java/io/codelex/flightplanner/configuration/SecurityConfig.java,4/3/43544a50be9e55398d7cf33b2724dc988e0e1661
---
+++ src/main/java/io/codelex/flightplanner/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package io.codelex.flightplanner.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/testing-api/**", "/api/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
